6a5bd8d34bfe2d21c4dc2352bea322a26273dbba
clangen2 <clangen2@users.noreply.github.com>
2020-07-10 15:51:44 -0700

    split up some tests

--- FastBridgeApp/test_main.py
+++ FastBridgeApp/test_main.py
@@ -33,10 +33,75 @@ def test_result_OvidPresent():
     assert response.status_code == 200
 
 
-def verify_text_integrity_ovid():
-    response = client.get("oracle/Latin/result/ovid_metamorphoses/start/end/10/dcc_core_list/start-end")
+def verify_text_integrity_ovid12():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/1.1/2.1/10/dcc_core_list/start-end")
     assert response.status_code == 200
     #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+def verify_text_integrity_ovid23():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/2.1/3.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+def verify_text_integrity_ovid34():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/3.1/4.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+def verify_text_integrity_ovid45():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/4.1/5.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+def verify_text_integrity_ovid56():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/5.1/6.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+def verify_text_integrity_ovid67():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/6.1/7.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+def verify_text_integrity_ovid78():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/7.1/8.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+
+
+def verify_text_integrity_ovid89():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/8.1/9.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+
+def verify_text_integrity_ovid910():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/9.1/10.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+
+def verify_text_integrity_ovid1011():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/10.1/11.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+
+def verify_text_integrity_ovid112():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/11.1/12.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+
+def verify_text_integrity_ovid1213():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/12.1/13.1/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
+def verify_text_integrity_ovid13end():
+    response = client.get("oracle/Latin/result/ovid_metamorphoses/13.1/end/10/dcc_core_list/start-end")
+    assert response.status_code == 200
+    #since oracle goes through pretty much every subsection of the text, if part of the text breaks, it will fail the test
+
 
 def verify_text_integrity_oxford():
     response = client.get("oracle/Latin/result/oxford_latin_course_college/start/end/10/dcc_core_list/start-end")
