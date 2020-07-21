19d776589dcd630b38f89ca5b01b1d31fd2567d0
clangen2 <clangen2@users.noreply.github.com>
2020-07-21 15:36:11 -0700

    updated about to pull from google sheets

--- FastBridgeApp/routers/about.py
+++ FastBridgeApp/routers/about.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, WebSocket, Request, File, Form, UploadFile, Depen
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import importlib
-
+import quickstart
 from pathlib import Path
 import DefinitionTools
 router = APIRouter()
@@ -18,31 +18,27 @@ def render_about(request: Request):
 @router.get("/texts")
 def about_the_texts(request: Request):
     context = {'request' : request}
-    return templates.TemplateResponse("about_lang_choice.html", context)
-
-@router.get("/texts/{language}")
-def about_the_texts_lang(request: Request, language: str):
-    context = {'request' : request}
-    the_text_names = importlib.import_module(f'data.{language}.texts').texts
+    data = quickstart.main() # this is a list of all the texts that is stored in a google spreadsheet. Each row/item of this list has 24 items, consult the sheet
+    table_headers =  data[0]
     books_data = []
-    for text in the_text_names:
-        computer_name =  text.lower().replace(" ", "_").replace(",","").replace(":","") #copy-pasted from add_new_text.py and should be – that is our gold standard for going from human names to machine names
-        the_text =  DefinitionTools.get_text(computer_name, language).book
-        if the_text.local_def:
-            bool1 = "Yes"
-        else:
-            bool1 = "No"
-        if the_text.local_lem:
-            bool2 = "Yes"
-        else:
-            bool2 = "No"
-        markup = f"<tr> <td>{text}</td><td>{the_text.subsections}</td><td>{bool1}</td><td>{bool2}</td></tr>"
-        to_add = {"values" : [text, the_text.subsections, bool1, bool2], "markup" : markup, "active" : True} #this is to make it searchable like the Lists result table, there will eventually be hundreds of texts.
+    for text in data[1:]:
+        markup = ['<tr>']
+        [markup.append(f"<td>{info}</td>") if info else markup.append(f"<td>No</td>") for info in text]
+        markup.append('</tr>')
+        markup= ''.join(markup)
+        #print(markup)
+        to_add= {"values" : text, "markup" : markup, "active" : True}
         books_data.append(to_add)
-        del the_text
+
+    columns = {}
+    headers = f''
+    for i in range(len(table_headers)):
+        print(table_headers[i])
+        columns[table_headers[i]] =  [True, True]
+        headers+=(f"<td onclick=\"sortTable(\'{table_headers[i]}\',{i})\">{table_headers[i]}</td>")
 
     context["books_data"] = books_data
-    context["language"] = language
-    context["columns"] = {"Title" : [True, True], "Subsection_Depth" : [True, True], "local_def" : [True, True], "local_lem" : [True, True]}
-    context["style"] = f'td{{max-width : 100px; width: 100px; color: white}}'
+    context["columns"] = columns
+    context["headers"] = headers
+    context["style"] = f'td{{max-width : 4vh; width: 4vh; color: white;}}'
     return templates.TemplateResponse("about_the_texts.html", context)
