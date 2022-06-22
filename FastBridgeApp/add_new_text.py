5e07dce9fe8000e274da1fad71779c8541553d36
janicezou <yzou@haverford.edu>
2022-06-22 10:40:49 -0400

    texts.py fixed in alphabetical order; updated apollonius_king_of_tyre.py, eutropius_breviarium_book_6.py, livy_ab_urbe_condita_ib_list_2_selections.py, section numbers from words to numbers

--- FastBridgeApp/add_new_text.py
+++ FastBridgeApp/add_new_text.py
@@ -83,11 +83,14 @@ def import_(title, section_level, csv, language, local_def=False, local_lem=Fals
     texts = importlib.import_module(f'data.{language}.texts')
     print(texts, " current content: section level")
     print(texts.texts)
-    print(texts, " current content: huamn to machine titles")
+    print(texts, " current content: human to machine titles")
     print(texts.textFileDict)
-    print(title)
+    print("uploading book:", title)
     texts.texts[title] =  section_level
     texts.textFileDict[title] =  filename
+    texts.texts = dict(sorted(texts.texts.items()))
+    texts.textFileDict = dict(sorted(texts.textFileDict.items()))
+    print("texts.py updated")
     print(texts.texts)
     print(texts.textFileDict)
     code = f'texts = {texts.texts} \n textFileDict = {texts.textFileDict}'
