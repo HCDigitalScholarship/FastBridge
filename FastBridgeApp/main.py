fa01b4285e834705e4fcc844f57dea5d0197d717
janicezou <yzou@haverford.edu>
2022-07-08 15:44:35 -0400

    error page

--- FastBridgeApp/main.py
+++ FastBridgeApp/main.py
@@ -18,24 +18,24 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from starlette.applications import Starlette
 
-# async def not_found(request, exc):
-#     context = {"request": request}
-#     context["statuscode"] = 404
-#     return templates.TemplateResponse("notfound.html", context)
+async def not_found(request, exc):
+    context = {"request": request}
+    context["statuscode"] = 404
+    return templates.TemplateResponse("notfound.html", context)
 
-# async def server_error(request, exc):
-#     context = {"request": request}
-#     print(exc)
-#     context["statuscode"] = 500
-#     return templates.TemplateResponse("notfound.html", context)
+async def server_error(request, exc):
+    context = {"request": request}
+    print(exc)
+    context["statuscode"] = 500
+    return templates.TemplateResponse("notfound.html", context)
 
-# exception_handlers = {
-#     404: not_found,
-#     500: server_error
-# }
+exception_handlers = {
+    404: not_found,
+    500: server_error
+}
 
-# app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, exception_handlers=exception_handlers)
-app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, exception_handlers=exception_handlers)
+# app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.include_router(lemmatize.router, prefix="/lemmatizer", tags=["lemmatize"])
 app.include_router(account.router, prefix = "/account", tags=["account"])
