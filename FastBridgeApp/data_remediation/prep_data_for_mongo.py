dcb61351df021b51cd15630ffd0a25c3ee9d2452
Sarah Keim <skeim@haverford.edu>
2024-05-28 11:56:24 -0400

    added csv conditional and fixed whitespace remvoal

--- FastBridgeApp/data_remediation/prep_data_for_mongo.py
+++ FastBridgeApp/data_remediation/prep_data_for_mongo.py
@@ -1,25 +1,46 @@
 import pandas as pd 
+from pathlib import PurePosixPath
 import os
+import argparse
+import sys
 
 def main():
-    convert_to_csv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", dest="folder",
+        help="current data folder", default="Texts_New")
+    options = parser.parse_args()
+    convert_to_csv(options.folder)
 
 '''
 Standardizes all column labels, removes unwanted columns, and changes current column labels to target column labels   
 '''
-def clean_data(folder, datasheet):
+def clean_data(folder, datasheet, is_csv):
 
-    possible_headers = ["HEAD_WORD", "LOCATION", "SECTION", "ORTHOGRAPHIC_FORM", "CASE", "GRAMMATICAL_SUBCATEGORY", "LASLA_SUBORDINATION_CODE", "LOCAL_DEFINITION", "LOCAL_PRINCIPAL_PARTS"]
-    remove_headers = ["GRAMMATICAL_CATEGORY", "_MERGE"]
-    target_headers = {"TITLE": "HEAD_WORD", "TEXT": "ORTHOGRAPHIC_FORM", "SUBORDINATION_CODE": "LASLA_SUBORDINATION_CODE", "LOCALDEF": "LOCAL_DEFINITION", "LOCAL DEFINITION": "LOCAL_DEFINITION", "RUNNINGCOUNT": "COUNTER", "GRAMMATICAL_CATEGORY_SUB": "GRAMMATICAL_SUBCATEGORY"}
+    possible_headers = ["head_word", "location", "section", "orthographic_form", "case", "grammatical_subcategory", "lasla_subordination_code", "local_definition", "local_principal_parts"]
+    remove_headers = ["grammatical_category", "_merge"]
+    target_headers = {"title": "head_word", 
+                      "headword": "head_word", 
+                      "text": "orthographic_form", 
+                      "orthographicform": "orthographic_form",
+                      "subordination_code": "lasla_subordination_code", 
+                      "laslasubordinationcode": "lasla_subordination_code",
+                      "partofspeech": "part_of_speech",
+                      "localdef": "local_definition", 
+                      "localdefinition": "local_definition", 
+                      "runningcount": "counter", 
+                      "grammatical_category_sub": "grammatical_subcategory",
+                      "grammaticalsubcategory": "grammatical_subcategory"}
 
-    text_data = pd.DataFrame(pd.read_excel(f"../data_remediation/Texts_New/{folder}/{datasheet}")) 
+    if is_csv:
+        text_data = pd.DataFrame(pd.read_csv(f"../data_remediation/{folder}/{datasheet}"))
+    else:
+        text_data = pd.DataFrame(pd.read_excel(f"../data_remediation/{folder}/{datasheet}")) 
     
     print("Original columns: ", text_data.columns)
+    text_data.columns = map(str.lower, text_data.columns)
+    text_data = text_data.rename(columns=lambda x: "".join(x.split(" ")))
     text_data = text_data.rename(columns=target_headers)
-    text_data.columns = map(str.upper, text_data.columns)
-    text_data.rename(columns=lambda x: x.strip(), inplace=True)
-    print("Renamed columns: ", text_data) 
+    print("Renamed columns: ", text_data.columns) 
     
     text_data_final = pd.DataFrame(columns=possible_headers)
 
@@ -44,18 +65,24 @@ def clean_data(folder, datasheet):
 '''
 Loops through all text data spreadsheets in local directory and calls data cleaning before converting to a csv file   
 '''
-def convert_to_csv():
-    for folder in os.listdir("../data_remediation/Texts_New/"):
-        for spreadsheet_name in os.listdir(f"../data_remediation/Texts_New/{folder}"):
+def convert_to_csv(folder):
+    for spreadsheet_name in os.listdir(f"../data_remediation/{folder}"):
+        
+        print("Current text spreadsheet: ", spreadsheet_name)
+    
+        if PurePosixPath(spreadsheet_name).suffix == ".csv":
+            text_data_final = clean_data(folder, spreadsheet_name, True)   
+            text_data_final.to_csv(f"Cleaned_Texts/{spreadsheet_name}",  
+                index = None, 
+                header=True)
+        else:
+            text_data_final = clean_data(folder, spreadsheet_name, False)
+            spreadsheet_csv_name = spreadsheet_name.strip(".xlsx")    
+            text_data_final.to_csv(f"Texts_New_csv/{spreadsheet_csv_name}.csv",  
+                index = None, 
+                header=True)
             
-            print("Current text spreadsheet: ", spreadsheet_name)
-
-            text_data_final = clean_data(folder, spreadsheet_name)
             
-            spreadsheet_csv_name = spreadsheet_name.strip(".xlsx")        
-            text_data_final.to_csv (f"Texts_New_csv/{spreadsheet_csv_name}.csv",  
-                    index = None, 
-                    header=True)
                     
 
 if __name__ == "__main__":
