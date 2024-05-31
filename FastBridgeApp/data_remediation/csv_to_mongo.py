361f74f91658e2c78681bbfa6198c1fafaf93c5b
clangen2 <carter.langen@gmail.com>
2024-05-31 18:05:43 +0000

    Adding changes to csv_to_mongo.py

--- FastBridgeApp/data_remediation/csv_to_mongo.py
+++ FastBridgeApp/data_remediation/csv_to_mongo.py
@@ -5,7 +5,7 @@ import json
 from subprocess import call
 
 # Define the directory containing CSV files and MongoDB details
-csv_directory = '/srv/FastBridge/FastBridgeApp/data_remediation/FOLDERNAME'
+csv_directory = '/srv/FastBridge/FastBridgeApp/data_remediation/Cleaned_FastBridge_Files_1'
 mongo_uri = 'mongodb://localhost:27017'
 database_name = 'Latin'
 collection_name = 'AllTexts'
@@ -20,12 +20,13 @@ def csv_to_json(csv_file_path):
 for csv_file in os.listdir(csv_directory):
     if csv_file.endswith('.csv'):
         csv_file_path = os.path.join(csv_directory, csv_file)
+        print(str(csv_file_path)+"\n")
         json_data = csv_to_json(csv_file_path)
 
         # Create a temporary JSON file to store the document
         temp_json_file = 'temp.json'
         with open(temp_json_file, 'w') as file:
-            json.dump({'filename': csv_file, 'data': json_data}, file)
+            json.dump([{'filename': csv_file, 'data': json_data}], file)
 
         # Import the JSON file into MongoDB
         call(['mongoimport', '--uri', mongo_uri, '--db', database_name, '--collection', collection_name, '--file', temp_json_file, '--jsonArray'])
