cf7eccb1c7ea2e3b7c6db9418856aee2a092a2bd
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-07-28 13:02:29 -0400

    removed data_remidiation/, refactored into data/

---
+++ FastBridgeApp/data/data_ct.py
@@ -0,0 +1,122 @@
+import os, sys
+from clean_upload import string_to_slug
+
+# Add project root to sys.path in order to import main
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import json
+from collections import defaultdict
+from pymongo import MongoClient
+
+from MongoDefinitionTools import mg_get_locations
+
+mongo_uri = os.getenv('ATLAS_URI')
+print(f"Connecting to MongoDB...")
+
+if not mongo_uri:
+    print("MongoDB URI not found. Ensure you're in the right dir and .env file is set up correctly.")
+    exit(1)
+    
+client = MongoClient(mongo_uri)
+
+def aggregate_field_counts(client, db_name, field_name, output_file="output_counts.json"):
+    """
+    Aggregate counts of a specified field across all collections in a MongoDB database.
+    Counts how many times each value appears, counting duplicates in collections.
+
+    Args:
+        client: MongoClient instance.
+        db_name (str): Name of the MongoDB database.
+        field_name (str): The field name to aggregate counts for.
+        output_file (str, optional): JSON file path to save the results. Defaults to "output_counts.json".
+    """
+    db = client[db_name]
+    counts = defaultdict(int)
+
+    for collection_name in db.list_collection_names():
+        collection = db[collection_name]
+        for doc in collection.find({}, {field_name: 1}):
+            key = doc.get(field_name)
+            if key:
+                counts[key] += 1
+
+    counts = dict(sorted(counts.items(), key=lambda x: x[1], reverse=True))
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(counts, f, ensure_ascii=False, indent=2)
+
+    print(f"Saved aggregated counts to {output_file}")
+
+def get_sections(language):
+    """
+    Get sections for a given language from the title renaming dictionary.
+    
+    Args:
+        language (str): The language to get sections for.
+        
+    Returns:
+        dict: A dictionary with section names as keys and their corresponding locations as values.
+    """
+
+    db = client[f"{language}-Texts"]
+    
+    # Get sections based on the title renaming dictionary
+    new_sections = {}
+    for text in db.list_collection_names():
+        text = text.split('_')[0]
+        new_sections[text] = mg_get_locations(language, string_to_slug(text))
+        
+    # Read current sections
+    try:
+        with open('FastBridgeApp/data/Static/sections.json', 'r', encoding='utf-8') as f:
+            sections = json.load(f)
+    except FileNotFoundError:
+        sections = {}
+        print("sections.json not found, creating a new one.")
+
+    sections[language] = new_sections
+    
+    # Save sections to JSON
+    if not os.path.exists('FastBridgeApp/data/Static'):
+        os.makedirs('FastBridgeApp/data/Static')
+        
+    print("Saving sections to FastBridgeApp/data/Static/sections.json")
+    with open('FastBridgeApp/data/Static/sections.json', 'w', encoding='utf-8') as f:
+        json.dump(sections, f, ensure_ascii=False, indent=4)
+    print("Sections saved to FastBridgeApp/data/Static/sections.json")
+
+    return sections
+
+def update_render_cache(language):
+    """
+    Update the render cache for titles in a given language.
+    
+    Args:
+        language (str): The language to update the cache for.
+        
+    Returns:
+        None
+    """
+    from MongoDefinitionTools import get_title_location_levels, render_titles
+
+    cache_path = f"FastBridgeApp/data/Static/{language}_titles.json"
+
+    print("Making new cache for", language)
+    title_location_levels = get_title_location_levels(language, depth=True)
+    titles = render_titles(title_location_levels)
+    titles2 = render_titles(title_location_levels, dropdown="2")
+    with open(cache_path, "w", encoding="utf-8") as f:
+        json.dump({"titles": titles, "titles2": titles2}, f, ensure_ascii=False, indent=2)
+
+    return titles, titles2
+
+aggregate_field_counts(client, "Latin-Texts", "head_word", output_file="FastBridgeApp/data/Static/latin_headword_counts.json")
+aggregate_field_counts(client, "Greek-Texts", "head_word", output_file="FastBridgeApp/data/Static/greek_headword_counts.json")
+
+get_sections("Latin")
+get_sections("Greek")
+
+update_render_cache("Latin")
+update_render_cache("Greek")
---
+++ FastBridgeApp/data/prep_data_for_mongo.py
@@ -0,0 +1,173 @@
+import pandas as pd 
+from pathlib import PurePosixPath
+import os
+import argparse
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", dest="data_folder",
+        help="current data folder", default="Texts_New")
+    parser.add_argument("-d", dest="data_type",
+        help="specify text or dictionary data", default="text")
+    options = parser.parse_args()
+    convert_to_csv(options.data_folder, options.data_type)
+
+
+
+def convert_to_csv(data_folder, data_type):
+    '''
+    Loops through all text data spreadsheets in local directory and calls data cleaning before converting to a csv file (if not already a csv file) 
+    '''
+
+    if data_type == "text":
+        is_text_data = True
+    elif data_type == "dict" or data_type == "dictionary":
+        is_text_data = False
+    else:
+        raise argparse.ArgumentTypeError('Invalid value!')
+    acceptable_headers, target_headers, necessary_headers = data_config(is_text_data)
+
+    for dirpath, dirnames, filenames in os.walk(f"../data/{data_folder}"):
+        for spreadsheet_name in filenames:
+            print("Current Spreadsheet: ", spreadsheet_name)
+
+            if PurePosixPath(spreadsheet_name).suffix == ".csv":
+                data = pd.DataFrame(pd.read_csv(f"../data/{data_folder}/{spreadsheet_name}"))
+                cleaned_data_final = clean_data(data, spreadsheet_name, acceptable_headers, target_headers, necessary_headers, is_text_data)
+                cleaned_data_final.to_csv(f"Cleaned_Texts/{spreadsheet_name}",  
+                index = None, 
+                header=True)
+            elif PurePosixPath(spreadsheet_name).suffix == ".xlsx":
+                data = pd.DataFrame(pd.read_excel(f"../data/{data_folder}/{spreadsheet_name}")) 
+                cleaned_data_final = clean_data(data, spreadsheet_name, acceptable_headers, target_headers, necessary_headers, is_text_data)
+                spreadsheet_csv_name = spreadsheet_name.strip(".xlsx")    
+                cleaned_data_final.to_csv(f"Texts_New_csv/{spreadsheet_csv_name}.csv",  
+                    index = None, 
+                    header=True)
+                
+
+
+def data_config(is_text_data):
+    '''
+    Assigns acceptable and target headers for data to be compared against 
+    '''
+
+    necessary_headers = ["head_word", "location", "sentence", "counter", "orthographic_form"]
+    if is_text_data:
+        data_acceptable_headers = ["head_word", "location", "sentence", "counter", "orthographic_form", "case", "grammatical_subcategory", "lasla_subordination_code", "local_definition", "local_principal_parts"]
+        data_target_headers = {
+                    "title": "head_word", 
+                    "headword": "head_word", 
+                    "text": "orthographic_form", 
+                    "section": "sentence",
+                    "orthographicform": "orthographic_form",
+                    "subordination_code": "lasla_subordination_code", 
+                    "laslasubordinationcode": "lasla_subordination_code",
+                    "partofspeech": "part_of_speech",
+                    "localdef": "local_definition", 
+                    "localdefinition": "local_definition", 
+                    "runningcount": "counter", 
+                    "grammatical_category_sub": "grammatical_subcategory",
+                    "grammaticalsubcategory": "grammatical_subcategory"}
+        return data_acceptable_headers, data_target_headers, necessary_headers
+    else:
+        dict_data_acceptable_headers = ["head_word", "lilaLemma", "principal_parts", "principal_parts_no_diacriticals", "simple_lemma", "gloss", "definition", "local_definition", "part_of_speech", 
+                                        "logeion_link", "forcellini_link", "conjugation", "declension", "proper", "regular", "stopword", "corpus_freq", "lasla_combined"]
+        dict_data_target_headers = {
+                    "title": "head_word", 
+                    "headword": "head_word", 
+                    "short_definition": "gloss",
+                    "shortdefinition": "gloss",
+                    "long_definition": "definition",
+                    "longdefinition": "definition",
+                    "local_principal_parts": "principal_parts"}
+        return dict_data_acceptable_headers, dict_data_target_headers, necessary_headers
+            
+
+
+def clean_data(data, datasheet, acceptable_headers, target_headers, necessary_headers, is_text_data):
+    '''
+    Standardizes all column labels, removes unwanted columns, and changes current column labels to target column labels   
+    '''
+
+    print("Original Columns:\n", data.columns)
+    data.columns = map(str.lower, data.columns)
+    data = data.rename(columns=lambda x: x.rstrip())
+    data = data.rename(columns=lambda x: x.replace(" ", "_"))
+    data = data.rename(columns=target_headers)
+    print("Renamed columns:\n", data.columns) 
+    
+    cleaned_data_final = pd.DataFrame(columns=necessary_headers)
+
+    cleaned_data = check_for_duplicates(datasheet, data)
+
+    if not is_text_data and "principal_parts" in cleaned_data.columns:
+        cleaned_data = add_no_diacriticals_column(cleaned_data)
+
+    for header in cleaned_data_final.columns:
+        if header in cleaned_data.columns:
+            cleaned_data_final[header] = cleaned_data[header].copy()
+
+    for header in cleaned_data.columns:
+        if header in acceptable_headers:
+            cleaned_data_final[header] = cleaned_data[[header]].copy()
+
+    if cleaned_data_final['sentence'].dtype == object:  
+        cleaned_data_final['sentence'] = cleaned_data_final['sentence'].astype(int) 
+        
+    print("Final columns:\n", cleaned_data_final)
+
+    return cleaned_data_final
+
+
+def check_for_duplicates(spreadsheet_name, data):
+    '''
+    Checks for both column duplicates and row duplicates (as well as entire rows/columns with NaN values), removes them, and logs them in the folder duplicate_data
+    '''
+
+    column_header_duplicates = data.columns.duplicated(keep=False)
+    if column_header_duplicates.any():
+        duplicate_columns = data.iloc[:, column_header_duplicates]
+        print("Duplicate Columns:\n", duplicate_columns)
+        if not os.path.exists(f"../data/duplicate_data/"):
+            os.mkdir(f"../data/duplicate_data/")
+        if not os.path.exists(f"../data/duplicate_data/duplicate_columns/"):
+            os.mkdir(f"../data/duplicate_data/duplicate_columns/")
+        spreadsheet_csv_name = spreadsheet_name.strip(".xlsx")
+        f = open(f"duplicate_data/duplicate_columns/{spreadsheet_csv_name}.csv", "w")
+        f.write(f"{spreadsheet_csv_name}\n")
+        f.write(duplicate_columns.to_csv(na_rep="NaN", index=False))
+        f.close()
+        data = data.dropna(axis=1, how="all")
+        dropped_columns = [col for col in data if col not in data.columns]
+        print("Dropped Columns: ", dropped_columns)
+
+    row_duplicates = data.duplicated(keep=False)
+    if row_duplicates.any():
+        duplicate_rows = data[row_duplicates]
+        print("Duplicate Rows: ", duplicate_rows)
+        if not os.path.exists(f"../data/duplicate_data/"):
+            os.mkdir(f"../data/duplicate_data/")
+        if not os.path.exists(f"../data/duplicate_data/duplicate_rows/"):
+            os.mkdir(f"../data/duplicate_data/duplicate_rows/")
+        spreadsheet_csv_name = spreadsheet_name.strip(".xlsx")
+        f = open(f"duplicate_data/duplicate_rows/{spreadsheet_csv_name}.csv", "w")
+        f.write(f"{spreadsheet_csv_name}\n")
+        f.write(duplicate_rows.to_csv(na_rep="NaN"))
+        f.close()
+        data = data.drop_duplicates()
+    
+    return data
+
+
+def add_no_diacriticals_column(data):
+    no_diacriticals = []
+    for row in data["principal_parts"]:
+        replaced_diacriticals = row.replace("ā","a").replace("ē","e").replace("ī","i").replace("ō","o").replace("ū","u").replace("ȳ","y").replace("Ā","A").replace("Ē","E").replace("Ī","I").replace("Ō","O").replace("Ū","U").replace("ë","e")
+        no_diacriticals.append(replaced_diacriticals)
+    data = data.assign(principal_parts_no_diacriticals=no_diacriticals)
+    return data
+
+if __name__ == "__main__":
+    main()
