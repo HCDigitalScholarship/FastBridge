71d9a23e4752c12b6fc5076b5c1d001f4a18ecba
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-08-05 15:10:06 -0400

    added demacronizer + minor background image fix for formulas page

--- FastBridgeApp/main.py
+++ FastBridgeApp/main.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI, WebSocket, Request, File, Form, UploadFile, Depends, HTTPException, status
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-import importlib
-import DefinitionTools
 from pathlib import Path
-from pydantic import BaseModel
-import add_new_text
 import uvicorn
-import secrets
-from fastapi.responses import RedirectResponse
-from typing import Optional
 from routers.ToolsApp import lemmatize
 from routers.sql_app import account
 from routers import oracle, select, about, user_help,export, stats
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from starlette.applications import Starlette
 
 async def not_found(request, exc):
     context = {"request": request}
@@ -35,7 +26,6 @@ exception_handlers = {
 }
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, exception_handlers=exception_handlers)
-# app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.include_router(lemmatize.router, prefix="/lemmatizer", tags=["lemmatize"])
 app.include_router(account.router, prefix = "/account", tags=["account"])
@@ -53,15 +43,15 @@ static_path = app_path / "static" / "assets"
 app.mount("/assets", StaticFiles(directory=static_path), name="assets")
 
 plot_path = static_path / "plots"
-app.mount("/plots", StaticFiles(directory=plot_path), name="plots")#for stats
+app.mount("/plots", StaticFiles(directory=plot_path), name="plots")
 
 @app.get("/")
 async def index(request : Request):
-    print(request['path'])
     return templates.TemplateResponse("list-index.html", {"request": request})
-    #buttons clicked on this page will take us to /select/{language}
 
+@app.get("/demacronizer", response_class=HTMLResponse)
+async def index(request : Request):
+    return templates.TemplateResponse("demacronizer.html", {"request": request})
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload = True, host="64.227.97.179")
- #End of select (or main?) code
