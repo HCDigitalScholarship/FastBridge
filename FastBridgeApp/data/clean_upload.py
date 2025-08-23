28a0891d9c55856640540ac9c531d559a4dd600d
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-08-23 00:00:05 -0400

    small fix for correct dictionary name display when uploading dictionaries

--- FastBridgeApp/data/clean_upload.py
+++ FastBridgeApp/data/clean_upload.py
@@ -173,12 +173,18 @@ def update_problematic_texts(file_name: str, issues: list):
     problematic_texts[file_name].extend(issues)
 
 def import_dataframe_to_mongo(db: MongoClient, df: pd.DataFrame, collection_name: str, chunk_size: int = 100000):
-    name_map = {
-        name.split("_")[0]: name
-        for name in db.list_collection_names()
-    }
-
-    first_part = collection_name.split("_")[0]
+    if not dict_db:
+        name_map = {
+            name.split("_")[0]: name
+            for name in db.list_collection_names()
+        }
+    else:
+        name_map = {
+            name: name
+            for name in db.list_collection_names()
+            if not name.startswith("bridge_")
+        }
+    first_part = collection_name.split("_")[0] if not dict_db else collection_name
     if first_part in name_map:
         actual_name_in_db = name_map[first_part]
         user_input = input(
